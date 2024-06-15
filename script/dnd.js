e9350de6730eaeaefabbe9d7984cc7d7a5d5f70d
Daniel Glória <jo_tape_97@hotmail.com>
2024-06-15 23:30:40 +0100

    update

--- script/dnd.js
+++ script/dnd.js
@@ -1,26 +1,29 @@
+async function fetchData(file) {
+    try {
+        const response = await fetch('./database/dnd/blank.json');
+        if(!response.ok) {
+            throw new Error ("HTTP error " + response.status);
+        }
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
 function writeData() {
     if (location.hash) {
-        var file = "./database/dnd/" + location.hash.replace("#", "") + ".json";
+        var file = './database/dnd/' + location.hash.replace("#", "") + '.json';
     }
     else {
-        var file = "./database/dnd/blank.json";
-    }
+        var file = './database/dnd/blank.json';
+    };
 
-    fetch(file)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error ("HTTP error " + response.status);
-            }
-            return response.json();
-        })
-        .then((json) =>
-            data = json)
-        .catch(function () {
-            dataError = true;
-        })
+    fetchData('./database/dnd/blank.json');
 
-    document.getElementById('name').innerHTML = data.name;
-    document.getElementById('level').innerHTML = data.leve;
-    document.getElementById('race').innerHTML = data.race;
-    document.getElementById('class').innerHTML = data.class;
+    document.getElementById('name').innerHTML = "a";
+    document.getElementById('level').innerHTML = "a";
+    document.getElementById('race').innerHTML = "a";
+    document.getElementById('class').innerHTML = "a";
 }
